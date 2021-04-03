4eeabacef8831f684af081218a54c5eca874382d
eldrinch <eldrinc@hotmail.com>
2021-04-03 13:03:42 -0300

    Changes from day 7

--- DeveloperSkills/script.js
+++ DeveloperSkills/script.js
@@ -160,7 +160,8 @@ const data2 = [12, 5, -5, 0, 4];
 const printForecast = function(arr) {
     let tmp = '';
     for (let i = 0; i < arr.length; i++) {
-        tmp = tmp + ` ${arr[i]}ºC in ${i + 1} days ...`;
+        //tmp = tmp + ` ${arr[i]}ºC in ${i + 1} days ...`;
+        tmp += ` ${arr[i]}ºC in ${i + 1} days ...`;
     }
 
     console.log('...' + tmp);
